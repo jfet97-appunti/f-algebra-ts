f7bcf317acbec740cdfbc9b9e5eb86a3efd58a64
Andrea Simone Costa <andrysimo1997@gmail.com>
2021-07-27 19:47:58 +0200

    remove unnecessary type-hints

--- algebra/index.ts
+++ algebra/index.ts
@@ -8,54 +8,54 @@ const listOfNumbers: List<number> = cons(5, cons(4, cons(3, cons(2, cons(1, nil)
 // ArrayAlgebra test
 pipe(
   listOfNumbers,
-  cata(listf, ArrayAlgebra<number>()),
-  (ns: Array<number>) => console.log(ns)
+  cata(listf, ArrayAlgebra()),
+  (ns) => console.log(ns)
 )
 
 // SumAlgebra test
 pipe(
   listOfNumbers,
   cata(listf, SumAlgebra<number>()),
-  (n: number) => console.log(n)
+  (n) => console.log(n)
 )
 
 // ProdAlgebra test
 pipe(
   listOfNumbers,
-  cata(listf, ProdAlgebra<number>()),
-  (n: number) => console.log(n)
+  cata(listf, ProdAlgebra()),
+  (n) => console.log(n)
 )
 
 // StringAlgebra test
 pipe(
   listOfNumbers,
-  cata(listf, StringAlgebra<number>()),
-  (s: string) => console.log(s)
+  cata(listf, StringAlgebra()),
+  (s) => console.log(s)
 )
 
 // ProjectionAlgebra test
 pipe(
   listOfNumbers,
   cata(listf, ProjectionAlgebra<number>()(x => x * 10)),
-  (ns: List<number>) => ns,
-  cata(listf, ArrayAlgebra<number>()),
-  (ns: Array<number>) => console.log(ns)
+  (ns) => ns,
+  cata(listf, ArrayAlgebra()),
+  (ns) => console.log(ns)
 )
 
 // ProjectionAlgebra test
 pipe(
   listOfNumbers,
-  cata(listf, ProjectionAlgebra<number>()(String)),
-  (ss: List<string>) => ss,
-  cata(listf, ArrayAlgebra<string>()),
-  (ns: Array<string>) => console.log(ns)
+  cata(listf, ProjectionAlgebra()(String)),
+  (ss) => ss,
+  cata(listf, ArrayAlgebra()),
+  (ss) => console.log(ss)
 )
 
 // ProjectionAlgebra test
 pipe(
   listOfNumbers,
-  cata(listf, PredicateAlgebra<number>(x => Boolean(x % 2))),
-  (ns: List<number>) => ns,
-  cata(listf, ArrayAlgebra<number>()),
-  (ns: Array<number>) => console.log(ns)
+  cata(listf, PredicateAlgebra(x => Boolean(x % 2))),
+  (ns) => ns,
+  cata(listf, ArrayAlgebra()),
+  (ns) => console.log(ns)
 )
